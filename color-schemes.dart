30267442e806d3946e82a1013731ed96cc12aa66
ABC <70518092+Anonymous-ABC-XYZ@users.noreply.github.com>
2025-06-03 20:27:24 +0530

    Add files via upload

---
+++ color-schemes.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const lightColorScheme = ColorScheme(
+  brightness: Brightness.light,
+  primary: Color(0xff7287fd),
+  onPrimary: Color(0xff151421),
+  primaryContainer: Color(0xffFAF9F6),
+  onPrimaryContainer: Color(0xffFAF9F6),
+  secondary: Color(0xff1e66f5),
+  onSecondary: Color(0xff1e66f5),
+  error: Color(0xFFd20f39),
+  onError: Color(0xFFe64553),
+  surface: Color(0xffFAF9F6),
+  onSurface: Color(0xffFAF9F6),
+);
+const darkColorScheme = ColorScheme(
+  brightness: Brightness.dark,
+  primary: Color(0xffB4BEFE),
+  onPrimary: Color(0xffFAF9F6),
+  primaryContainer: Color(0xff151421),
+  onPrimaryContainer: Color(0xffFAF9F6),
+  secondary: Color(0xff365DD7),
+  onSecondary: Color(0xffFAF9F6),
+  error: Color(0xffE2252B),
+  onError: Color(0xffd5212E),
+  surface: Color(0xff000000),
+  onSurface: Color(0xffFAF9F6),
+);
